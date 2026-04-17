0ce2f71083163b3767ed4f27699fc724b2e47f8f
AuroBreeze <1732373074@aliyun.com>
2026-04-17 21:10:18 +0800

    update: alt_alloc

--- arceos/modules/bump_allocator/src/lib.rs
+++ arceos/modules/bump_allocator/src/lib.rs
@@ -2,6 +2,8 @@
 
 use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
 
+const PAGE_SIZE: usize = 0x1000;
+
 /// Early memory allocator
 /// Use it before formal bytes-allocator and pages-allocator can work!
 /// This is a double-end memory range:
@@ -16,17 +18,31 @@ use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
 /// When it goes down to ZERO, free bytes-used area.
 /// For pages area, it will never be freed!
 ///
-pub struct EarlyAllocator<const SIZE: usize> {}
+pub struct EarlyAllocator<const SIZE: usize> {
+    bytes_start: usize,
+    b_pos: usize,
+    p_pos: usize,
+    pages_end: usize,
+}
 
 impl<const SIZE: usize> EarlyAllocator<SIZE> {
     pub const fn new() -> Self {
-        Self {}
+        Self {
+            bytes_start: 0,
+            b_pos: 0,
+            p_pos: SIZE - PAGE_SIZE,
+            pages_end: SIZE,
+        }
     }
 }
 
 impl<const SIZE: usize> BaseAllocator for EarlyAllocator<SIZE> {
     fn init(&mut self, start: usize, size: usize) {
-        todo!()
+        self.bytes_start = start;
+        self.b_pos = start;
+        self.p_pos = start + size - PAGE_SIZE;
+        self.pages_end = start + size;
+        // todo!()
     }
 
     fn add_memory(&mut self, start: usize, size: usize) -> allocator::AllocResult {
@@ -39,11 +55,17 @@ impl<const SIZE: usize> ByteAllocator for EarlyAllocator<SIZE> {
         &mut self,
         layout: core::alloc::Layout,
     ) -> allocator::AllocResult<core::ptr::NonNull<u8>> {
-        todo!()
+        let need_size = layout.size();
+        self.b_pos += need_size;
+
+        allocator::AllocResult::Ok(core::ptr::NonNull::new(self.b_pos as *mut u8).unwrap())
+        // todo!()
     }
 
     fn dealloc(&mut self, pos: core::ptr::NonNull<u8>, layout: core::alloc::Layout) {
-        todo!()
+        let need_size = layout.size();
+        self.b_pos -= need_size;
+        // todo!()
     }
 
     fn total_bytes(&self) -> usize {
